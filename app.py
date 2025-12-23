6ae40fc0dc6d2a363eb008d8c7e3e3a09db5638b
Pham Duy Khanh <phamduykhanh31082006@gmail.com>
2025-12-23 21:13:51 +0700

    remove excel, fix render deploy

--- app.py
+++ app.py
@@ -1,37 +1,14 @@
 from flask import Flask, render_template, request
-from openpyxl import Workbook, load_workbook
-import os
 from datetime import datetime
 import requests
 
-
 app = Flask(__name__)
 
-EXCEL_FILE = "data.xlsx"
 
-def save_to_excel(data):
-    if not os.path.exists(EXCEL_FILE):
-        wb = Workbook()
-        ws = wb.active
-        ws.append([
-            "Thời gian",
-            "Họ tên",
-            "Số điện thoại",
-            "Email",
-            "Link Shopee / MXH"
-        ])
-        wb.save(EXCEL_FILE)
-
-    wb = load_workbook(EXCEL_FILE)
-    ws = wb.active
-    ws.append(data)
-    wb.save(EXCEL_FILE)
 
 @app.route("/", methods=["GET"])
 def home():
     return render_template("index.html")
-
-import requests
 
 @app.route("/register", methods=["POST"])
 def register():
